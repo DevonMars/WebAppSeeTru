eb045d074caeae9ace72ffac58ea2c0a8fb382c1
Unknown <devon_marsham_16@live.nl>
2019-06-05 23:45:48 +0200

    started on Streams list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { StreamsComponent } from './streams/streams.component';
+import { StreamsListComponent } from './streams/streams-list/streams-list.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    StreamsComponent,
+    StreamsListComponent
   ],
   imports: [
     BrowserModule,
