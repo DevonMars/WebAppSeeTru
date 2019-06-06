e7b3c700ce582e04c43460955fd7e4486ba0baef
Unknown <devon_marsham_16@live.nl>
2019-06-06 10:59:09 +0200

    Added home component to replace Stream component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { StreamsComponent } from './streams/streams.component';
-import { StreamsListComponent } from './streams/streams-list/streams-list.component';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    StreamsComponent,
-    StreamsListComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
