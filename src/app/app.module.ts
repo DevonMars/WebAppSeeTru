1ea020cd236bcc1324ca3e0b581687a14243bcf9
Sjoerd van der Bok <sabok@avans.nl>
2019-06-07 10:37:36 +0200

    First socket support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { StreamComponent } from './stream/stream.component';
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+
+import { AppComponent } from './app.component';
+import { StreamComponent } from './components/stream/stream.component';
+import { NavbarComponent } from './components/layout/navbar/navbar.component';
+
 import { StreamService } from './services/stream/stream.service';
-import { NavbarComponent } from './layout/navbar/navbar.component';
+import { MessageService } from './services/message/message.service';
+
+const config: SocketIoConfig = { url: 'http://localhost:5000', options: {} };
 
 @NgModule({
   declarations: [
@@ -18,9 +23,10 @@ import { NavbarComponent } from './layout/navbar/navbar.component';
     BrowserModule,
     AppRoutingModule,
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    SocketIoModule.forRoot(config)
   ],
-  providers: [StreamService],
+  providers: [StreamService, MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/stream/stream.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { StreamService } from '../../services/stream/stream.service';
+
+@Component({
+  selector: 'app-stream',
+  templateUrl: './stream.component.html',
+  styleUrls: ['./stream.component.styl']
+})
+export class StreamComponent implements OnInit {
+  streams = [];
+  activeStreams = [];
+  messages = [];
+
+  constructor(private _streamService: StreamService) { }
+
+  ngOnInit() {
+    return this._streamService.list()
+      .subscribe(
+        res => {
+          this.streams = res
+        },
+        err => {
+          console.log(err)
+        }
+      )
+  }
+
+  addToActiveStreams(stream: any) {
+    if (this.activeStreams.length <= 3) {
+      this.activeStreams.push(stream)
+      console.log(this.activeStreams)
+    } else {
+      console.log('Too many streams open')
+    }
+  }
+
+}
