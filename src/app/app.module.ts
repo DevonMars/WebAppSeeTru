ae9da0e62263ebcb1921e69abb08ceb6994ab09b
Jeroen Bottcher <jm.bottcher@student.avans.nl>
2019-06-11 10:06:16 +0200

    push to master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,20 @@ import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 import { AppComponent } from './app.component';
 import { StreamComponent } from './components/stream/stream.component';
 import { NavbarComponent } from './components/layout/navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 import { StreamService } from './services/stream/stream.service';
 import { MessageService } from './services/message/message.service';
 import { StreamDetailComponent } from './components/stream-detail/stream-detail.component';
+import { LoginComponent } from './components/authentication/login/login.component';
+import { RegisterComponent } from './components/authentication/register/register.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 const config: SocketIoConfig = { url: 'http://thecirclebackend.herokuapp.com', options: {} };
 
@@ -19,14 +29,25 @@ const config: SocketIoConfig = { url: 'http://thecirclebackend.herokuapp.com', o
     AppComponent,
     StreamComponent,
     NavbarComponent,
-    StreamDetailComponent
+    StreamDetailComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserModule,
     HttpClientModule,
-    SocketIoModule.forRoot(config)
+    SocketIoModule.forRoot(config),
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule
+    
   ],
   providers: [StreamService, MessageService],
   bootstrap: [AppComponent]
