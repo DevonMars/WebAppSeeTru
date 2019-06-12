57844fc5577faed593d57a2cff1b205efd31121e
jmcwater <jmcwater@avans.nl>
2019-06-12 10:46:04 +0200

    routes added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { StreamComponent } from './components/stream/stream.component';
+import { NavbarComponent } from './components/layout/navbar/navbar.component';
 
 const routes: Routes = [
+  {
+    path:'',
+    component: NavbarComponent
+  },
+  {
+    path:'*',
+    component: NavbarComponent
+  },
+  {
+    path:'login',
+    component: NavbarComponent
+  },
   {
     path:'stream',
     component: StreamComponent
