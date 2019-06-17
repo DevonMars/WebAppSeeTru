0a2471214cc28ab07cf9a8107385ac82e525c1f7
jmbottch <jmbottch@avans.nl>
2019-06-17 11:03:54 +0200

    fixing chat in profile

--- src/app/components/users/profile/profile.component.ts
+++ src/app/components/users/profile/profile.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/services/users/user.service';
 import { User } from 'src/app/models/user';
+import { StreamService } from 'src/app/services/stream/stream.service';
+import { MessageService } from 'src/app/services/message/message.service';
+import { Subscription } from 'rxjs';
+import { Message } from 'src/app/models/message';
+import { SignService } from 'src/app/services/sign/sign.service';
 
 @Component({
   selector: 'app-profile',
@@ -9,10 +14,18 @@ import { User } from 'src/app/models/user';
 })
 export class ProfileComponent implements OnInit {
 
+  message : Message
   user: User
+  messages: []
+  private _msgSub : Subscription
+  messagetxt : String
+  disableButton = false
 
   constructor(
-    private _userService : UserService
+    private _userService : UserService,
+    private _streamService : StreamService,
+    private msgService : MessageService,
+    private _sign : SignService
   ) { }
 
   ngOnInit() {
@@ -21,12 +34,48 @@ export class ProfileComponent implements OnInit {
     .subscribe(
       res => {
         this.user = res
-        console.log(this.user)
+        this.messages = this.user.messages
+        console.log(this.messages)
       },
       err => {
         console.log(err)
       }
-    )
+    ),
+    this.getMessages();
+    this._msgSub = this.msgService.messages.subscribe(() => {
+      this.getMessages();
+    });
   }
+
+  getMessages() {
+    this._streamService.getMessages(this.user._id).subscribe(
+      res => {
+        this.messages = res;
+        //.autoScroll();
+      }
+    );
+  }
+
+  sendMessage() {
+    this.message.message = this.messagetxt;
+    this.message.authorname = localStorage.getItem('username');
+    this.message.author = localStorage.getItem('userId');
+    this.message.host = this.user._id;
+    const signature = this._sign.signMessage(this.messagetxt);
+    this.message.signature = signature.signature;
+    this.msgService.newMessage(this.message).subscribe(
+      res => {
+        this.messagetxt = '';
+        this.disableButton = false;
+        console.log(res);
+      },
+      err => console.log(err)
+    );
+    // this.autoScroll();
+  }
+
+  // autoScroll() {
+  //   $(".scrollfield.chatbox").stop().animate({ scrollTop: $(".scrollfield.chatbox")[0].scrollHeight}, 1000);
+  // }
 
 }
